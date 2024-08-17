df9a4fc52685b64dec670a2ec745746e4aaefcfe
a.v.cherkashin <a.v.cherkashin@tinkoff.ru>
2024-08-17 19:37:14 +0500

    before_request request_id

--- apps/auth/main.py
+++ apps/auth/main.py
@@ -5,6 +5,7 @@ from fastapi.responses import ORJSONResponse
 from fastapi import FastAPI, Request, status
 from fastapi.responses import ORJSONResponse
 from fastapi import FastAPI
+
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
@@ -49,10 +50,13 @@ app.include_router(api_router)
 FastAPIInstrumentor.instrument_app(app)
 
 
-@app.middleware('http')
+@app.middleware("http")
 async def before_request(request: Request, call_next):
-    response = await call_next(request)
-    request_id = request.headers.get('X-Request-Id')
+    request_id = request.headers.get("X-Request-Id")
     if not request_id:
-        return ORJSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={'detail': 'X-Request-Id is required'})
-    return response
+        return ORJSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"detail": "X-Request-Id is required"})
+    tracer: trace.Tracer = trace.get_tracer(__name__)
+    with tracer.start_as_current_span("auth-api") as span:
+        span.set_attribute("http.request_id", request_id)
+        response = await call_next(request)
+        return response
